102992f908eedf4aeb192eff7107ec072bdcfa4b
RuhtraAerroc <102332708+RuhtraAerroc@users.noreply.github.com>
2022-05-24 08:50:15 -0400

    1.0.0.2
    NEW: Layout Master.
    Criação do Layout Master qualificado como padrão.

---
+++ resources/views/Layout/LayoutMaster.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/3a5cec123c.js" crossorigin="anonymous"></script>
+        <title>@yield('title')</title>
+    </head>
+    <body>
+        @include('layout.LayoutHeader')
+        @yield('content')
+        @include('layout.LayoutFooter')
+    </body>
+</html>
+
+

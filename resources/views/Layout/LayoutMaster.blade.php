63c2a4344c01241d2733f76514ac9784c3606d49
RuhtraAerroc <102332708+RuhtraAerroc@users.noreply.github.com>
2022-06-27 07:38:01 -0400

    3.4.0.3
    UPDATE: LayoutMaster.
    Atualização do LayoutMaster.

--- resources/views/Layout/LayoutMaster.blade.php
+++ resources/views/Layout/LayoutMaster.blade.php
@@ -4,7 +4,10 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="theme-color" content="#ffffff">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="icon" type="image/png" sizes="16x16"  href="storage\favicons\favicon-16x16.png">
         <script src="https://kit.fontawesome.com/3a5cec123c.js" crossorigin="anonymous"></script>
         @yield('title')
     </head>
